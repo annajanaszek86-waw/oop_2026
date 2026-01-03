7da7b41f3c18413d38cf47add6ea7bdc836fce25
annajanaszek86-waw <annajanaszek86@gmail.com>
2026-01-03 11:28:35 +0100

    Added imie to Czlowiek construktor

--- klasy.py
+++ klasy.py
@@ -2,15 +2,22 @@
 class Czlowiek:
     #istota
     gatunek = "Homo Sapiens"
-    def __init__(self):
+    def __init__(self, imie):
         #konstruktor
         #akt istnienia
-       print("Niech powstanie Człowiek")
+       print(f"Niech powstanie Człowiek o imieniu {imie}")
+       self.imie = imie
+        # adam.imie = "Adam"
+        # ewa.imie = "Ewa"
 
 #powstawanie obiektu
 #gotowanie z przepisu
-adam = Czlowiek()
+adam = Czlowiek("Adam")
+ewa = Czlowiek("Ewa")
 # print(type(adam))
 # print(dir(Czlowiek))
 # print(dir(adam))
 print(adam.gatunek)
+print(ewa.gatunek)
+print(adam.imie)
+print(ewa.imie)
